84f9a001aa801cb5c3ca3caac72d7b5fa9b47301
daniel diaz <danieldiaz132001@gmail.com>
2021-10-20 23:59:55 -0500

    da

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
 	const initialState = useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-			<BrowserRouter /tienda-daniel>
+			<BrowserRouter>
 				<Layout>
 					<Switch>
 						<Route exact path="/" component={Home} />
