5ca8a493515d839ff32365e58cad74248f65c28d
daniel diaz <danieldiaz132001@gmail.com>
2021-10-20 23:36:40 -0500

    repo1

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
+		filename: 'tienda-daniel/bundle.js',
 		publicPath: '/tienda-daniel/',
 	},
 	mode: 'development',
